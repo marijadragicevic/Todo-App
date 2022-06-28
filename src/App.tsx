f438da028b173688140dda0d13894d276f28dfc9
Marija Dragicevic <marija.dragicevic@vega.local>
2022-06-28 14:58:40 +0200

    feature/header added style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.scss';
+import './Style/App.scss';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Header.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Header: React.FC = () => {
+    return <h2 className='heading'>Todo App</h2>
+}
+
+export default Header;
