5a217992424915f944a4a55fe08009fa5f4fd00f
Marija Dragicevic <marija.dragicevic@vega.local>
2022-07-01 13:40:08 +0200

    feature/form added functionality to form

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,28 @@ import React, { useState } from 'react';
 import './Style/App.scss';
 import Header from './Components/Header';
 import Form from './Components/Form';
+import { Todo } from './Components/model';
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  }
+
+  console.log(todos);
+
+  console.log(todos);
 
   return (
     <div className="App">
       <Header />
-      <Form />
+      <Form todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 }
--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { AiOutlinePlus } from "react-icons/ai";
 
-const Form = () => {
+type Props = {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    // when function isn't returning (void)
+    handleAdd: (e: React.FormEvent) => void;
+}
+
+const Form: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
     return (
-        <form className='form'>
-            <input type='input' placeholder='Enter a task' required className='form__input' />
+        <form className='form' onSubmit={(e) => handleAdd(e)}>
+            <input
+                type='input'
+                placeholder='Enter a task'
+                required
+                className='form__input'
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)} />
             <button className='form__btn' type='submit'><AiOutlinePlus /></button>
         </form>
     )
